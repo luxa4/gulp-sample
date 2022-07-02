a92c499bb0a60c52142c7b39166925c8f033dbbd
luxa4 <luxa4ru@gmail.com>
2022-07-02 12:40:05 +0300

    add gulp configuration 3

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    indent: ['error', 2, { 'SwitchCase': 1 }],
+    'no-var': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    "comma-dangle": ["error", "never"],
+    "require-jsdoc" : 0
+  },
+};
